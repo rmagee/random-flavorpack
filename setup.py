4c8df9a19aadad5b6644225dd468b190f6ef7113
Serial Lab <slab@serial-lab.com>
2018-03-09 14:20:20 -0700

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='random_flavorpack',
-    version='0.2.0',
+    version='0.2.1',
     packages=['', 'api', 'tests', 'generators', 'migrations'],
     package_dir={'': 'random_flavorpack'},
     url='https://gitlab.com/serial-lab/random-flavorpack',
