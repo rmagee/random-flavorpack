e7e564a663b5baa2c776d7d22a6ddfc6eeb7efe3
Serial Lab <slab@serial-lab.com>
2018-03-09 14:31:29 -0700

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,18 @@ setup(
     package_dir={'': 'random_flavorpack'},
     url='https://gitlab.com/serial-lab/random-flavorpack',
     license='GPLv3',
-    author='Rob Magee',
+    author='SerialLab Corp',
     author_email='slab@serial-lab.com',
-    description='Randomized Number Flavorpack Plugin for SerialBox'
+    description='Randomized Number Flavorpack Plugin for SerialBox',
+    keywords=('seriallab, quartet, randomized, numbers, serialbox, '
+             'level-4 quartet'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: SerialBox',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
