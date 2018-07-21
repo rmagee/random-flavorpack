9c28525dba3cc971ce5df2e4bfb70f720ea0b052
Serial Lab <slab@serial-lab.com>
2018-07-21 09:58:12 -0600

    Fixed old comment headers.

--- random_flavorpack/api/routers.py
+++ random_flavorpack/api/routers.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright 2015 Rob Magee.  All rights reserved.
+# Copyright 2018 SerialLab, Corp..  All rights reserved.
 
 from rest_framework.routers import DefaultRouter
 from random_flavorpack.api.viewsets import RandomizedRegionViewSet
